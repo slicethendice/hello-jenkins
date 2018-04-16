a2b8120f0c69fc20b85445e6dc687caa80dead42
Jeremy Madrid <jmadrid@upsie.com>
2018-04-16 13:27:31 -0500

    Back to dog

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 var express = require('express');
- 
+
 var app = express();
- 
+
 app.get('/', function (req, res) {
-  res.send('hello world');
+  res.send('hello dog');
 });
- 
+
 app.listen(process.env.PORT || 5000);
- 
+
 module.exports = app;
