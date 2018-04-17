eb327efa80bb74ce9bbda69a6c1f8da71d873051
Jeremy Madrid <jmadrid@upsie.com>
2018-04-16 21:05:10 -0500

    dsfsd

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('hello goat');
+  res.send('hello cat');
 });
 
 app.listen(process.env.PORT || 5000);
