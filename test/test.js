3cb1b52de233cee1f0529b8681bb5e7bf4312bec
Jeremy Madrid <jmadrid@upsie.com>
2018-04-16 21:11:18 -0500

    sdfsd

--- test/test.js
+++ test/test.js
@@ -3,6 +3,6 @@ var app = require('../app.js');
 
 describe('GET /', function() {
   it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello cat', done);
+    request(app).get('/').expect('hello dolphin', done);
   });
 });
